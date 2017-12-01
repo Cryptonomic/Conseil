9e82ab03b308d438320b87300b064de2bfc8840d
Vishakh <vishakh@users.noreply.github.com>
2017-11-30 21:50:00 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "Conseil"
+version := "0.0.1"
+scalaVersion := "2.12.3"
+
+val akkaHttpVersion = "10.0.10"
+
+libraryDependencies  ++=  Seq(
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe" % "config" % "1.3.1",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2",
+  "org.scalaj" % "scalaj-http_2.12" % "2.3.0"
+)
---
+++ src/main/scala/tech/cryptonomic/EnableCORSDirectives.scala
@@ -0,0 +1,24 @@
+package tech.cryptonomic
+
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.headers.{`Access-Control-Allow-Credentials`, `Access-Control-Allow-Headers`, `Access-Control-Allow-Methods`, `Access-Control-Allow-Origin`}
+import akka.http.scaladsl.server.directives.RespondWithDirectives
+
+trait EnableCORSDirectives extends RespondWithDirectives {
+
+  private val allowedCorsVerbs = List(
+    CONNECT, DELETE, GET, HEAD, OPTIONS,
+    PATCH, POST, PUT, TRACE
+  )
+
+  private val allowedCorsHeaders = List(
+    "X-Requested-With", "content-type", "origin", "accept"
+  )
+
+  lazy val enableCORS =
+    respondWithHeader(`Access-Control-Allow-Origin`.`*`) &
+    respondWithHeader(`Access-Control-Allow-Methods`(allowedCorsVerbs)) &
+    respondWithHeader(`Access-Control-Allow-Headers`(allowedCorsHeaders)) &
+    respondWithHeader(`Access-Control-Allow-Credentials`(true))
+
+}
