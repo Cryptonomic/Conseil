6903996b0f89210204fee77aa76476e549434d55
Vishakh <vishakh@users.noreply.github.com>
2017-12-12 01:42:47 -0500

    Added SSL support and now only running over HTTPS.

--- src/main/scala/tech/cryptonomic/conseil/Conseil.scala
+++ src/main/scala/tech/cryptonomic/conseil/Conseil.scala
@@ -1,7 +1,11 @@
 package tech.cryptonomic.conseil
 
+import java.io.InputStream
+import java.security.{KeyStore, SecureRandom}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
+import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
@@ -9,8 +13,8 @@ import com.typesafe.scalalogging.LazyLogging
 import tech.cryptonomic.conseil.directives.EnableCORSDirectives
 import tech.cryptonomic.conseil.routes.Tezos
 import tech.cryptonomic.conseil.util.SecurityUtil
-
 import akka.http.scaladsl.model.StatusCodes._
+import com.typesafe.sslconfig.akka.AkkaSSLConfig
 
 import scala.concurrent.ExecutionContextExecutor
 
@@ -31,6 +35,7 @@ object Conseil extends App with LazyLogging with EnableCORSDirectives {
   implicit val system: ActorSystem = ActorSystem("conseil-system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+  val sslConfig = AkkaSSLConfig()
 
   val route = enableCORS {
     validateApiKey { apiKey =>
@@ -40,7 +45,31 @@ object Conseil extends App with LazyLogging with EnableCORSDirectives {
     }
   }
 
+  // Begin HTTPS setup
+  // Drawn from https://doc.akka.io/docs/akka-http/10.0.11/scala/http/server-side/server-https-support.html#ssl-config
+
+  val password: Array[Char] = conf.getString("security.ssl.keystore-password").toCharArray
+  val ks: KeyStore = KeyStore.getInstance("PKCS12")
+  val keystore: InputStream = getClass.getClassLoader.getResourceAsStream("conseil.p12")
+
+  require(keystore != null, "Keystore required!")
+  ks.load(keystore, password)
+
+  val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+  keyManagerFactory.init(ks, password)
+
+  val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+  tmf.init(ks)
+
+  val sslContext: SSLContext = SSLContext.getInstance("TLS")
+  sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
+  val https: HttpsConnectionContext = ConnectionContext.https(sslContext)
+
+  // End HTTPS setup
+
+  Http().setDefaultServerHttpContext(https)
   val bindingFuture = Http().bindAndHandle(route, conseil_hostname, conseil_port)
+  println(System.getProperty("user.dir"))
   println(s"Bonjour..")
   while(true){}
   bindingFuture
