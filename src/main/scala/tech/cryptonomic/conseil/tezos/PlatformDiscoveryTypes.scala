39146c08f5121698efffeb58b42a7e0a7cc239ef
piotrkosecki <piotr.kosecki@gmail.com>
2018-11-21 14:44:44 +0100

    added javadoc comments to platform discovery types

--- src/main/scala/tech/cryptonomic/conseil/tezos/PlatformDiscoveryTypes.scala
+++ src/main/scala/tech/cryptonomic/conseil/tezos/PlatformDiscoveryTypes.scala
@@ -10,14 +10,19 @@ import tech.cryptonomic.conseil.tezos.PlatformDiscoveryTypes.KeyType.KeyType
   */
 object PlatformDiscoveryTypes {
 
+  /** Class required for DataType enum serialization */
   class DataTypeRef extends TypeReference[DataType.type]
 
+  /** Class required for KeyType enum serialization */
   class KeyTypeRef extends TypeReference[KeyType.type]
 
+  /** Case class representing network */
   final case class Network(name: String, displayName: String, platform: String, network: String)
 
+  /** Case class representing single entity of a given network */
   final case class Entity(name: String, displayName: String, count: Int, network: String)
 
+  /** Case class representing single attribute of given entity from DB */
   final case class Attributes(
     name: String,
     displayName: String,
@@ -27,16 +32,23 @@ object PlatformDiscoveryTypes {
     entity: String
   )
 
+  /** Enumeration of data types */
   object DataType extends Enumeration {
     type DataType = Value
     val Enum, Hex, Binary, Date, DateTime, String, Int, LargeInt, Decimal, Boolean = Value
   }
 
+  /** Enumeration of key types */
   object KeyType extends Enumeration {
     type KeyType = Value
     val NonKey, UniqueKey = Value
   }
 
+  /**
+    * Function maps slick column type as a string to DataType
+    * @param tpe type as a string from slick TableQuery
+    * @return one of DataType enums
+    */
   def mapType(tpe: String): DataType = {
     val optionRegex = "Option\\[([A-Za-z0-9']+)\\]".r
     tpe match {
