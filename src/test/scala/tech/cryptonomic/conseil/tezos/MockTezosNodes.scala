5ca6db7a4eee38336704eb039ef718b501917870
Ivano Pagano <pagoda_5b@hotmail.com>
2018-11-22 18:54:39 +0100

    Change mock scenarios to be defined in terms of level vs. time of call
    WIP: in each scenario the forking code will require data from different
     block hashes other than the current head, so the node should be ready
     to handle those cases too, looking at files that are currently missing

--- src/test/scala/tech/cryptonomic/conseil/tezos/MockTezosNodes.scala
+++ src/test/scala/tech/cryptonomic/conseil/tezos/MockTezosNodes.scala
@@ -11,32 +11,41 @@ import scala.util.Try
   *
   * In each scenario we can imagine a "snapshot" of the node and the results
   * that it is expected to return from the block request, based on the
-  * exact time-frame (Tn) and branch considered the "main" one at that time
+  * exact level (Ln) for that time-frame and the corresponding "main" branch
   *
   * Most snapshot for the same time-frame will return the same results. It
   * doesn't needs to be so, but it simplifies the data definition
   *
   * SCENARIO 1: no fork
+  * - time ->
   *
-  * -----[T1]---------------  branch-0
+  *
+  * -----[L2]---------------  branch-0
   *
   *
   * SCENARIO 2: single fork
+  * - time ->
   *
-  *             -----------[T3]----------  branch-1
-  * -----[T1]--/---[T2]------------------  branch-0
   *
-  * SCENARIO 3: singel fork alternating with the original
+  *            |-----------[L5]----------  branch-1
+  * -----[L2]--|---[L4]------------------  branch-0
   *
-  *             -----------[T3]---------[T5]----  branch-1
-  * -----[T1]--/---[T2]------------[T4]---------  branch-0
+  *
+  * SCENARIO 3: single fork alternating with the original
+  * - time ->
+  *
+  *
+  *            |-----------[L5]----------[L7]------  branch-1
+  * -----[L2]--|---[L4]------------[L6]------------  branch-0
   *
   *
   * SCENARIO 4: two forks alternating with the original
+  * - time ->
   *
-  *             -------------------[T4]---------------  branch-2
-  *             -----------[T3]----------[T5]---------  branch-1
-  * -----[T1]--/---[T2]-------------------------[T6]--  branch-0
+  *
+  *            |-------------------[L6]---------------  branch-2
+  *            |-----------[L5]----------[L7]---------  branch-1
+  * -----[L2]--|---[L4]-------------------------[L8]--  branch-0
   *
   */
 object MockTezosNodes {
@@ -50,32 +59,32 @@ object MockTezosNodes {
 
   private val emptyBlockOperationsResult = Future.successful("[[]]")
 
-  /* expected hash based on branch and time-frame
+  /* expected hash based on branch and level
    * this must match the base file hash returned from the mock node when the head block is requested
    */
-  def getHeadHash(onBranch: Int, atTime: Int) = (onBranch, atTime) match {
-    case (_, 1) => "BKy8NcuerruFgeCGAoUG3RfjhHf1diYjrgD2qAJ5rNwp2nRJ9H4"
-    case (_, 2) => "BKpFANTnUBqVe8Hm4rUkNuYtJkg7PjLrHjkQaNQj7ph5Bi6qXVi"
-    case (_, 3) => "BLFaY9jHrkuxnQAQv3wJif6V7S6ekGHoxbCBmeuFyLixGAYm3Bp"
-    case (0, 4) => "BMSw4mdnPTRpNtNoRBGpnYDGGH9pcxB2FHBxQeonyUunaV5bAz1"
-    case (2, 4) => "BMKRY5YvFhbwcLPsV3vfvYZ97ktSfu2eJTx2V21PfUxUEYXzTsp"
-    case (_, 5) => "BM7bFA88UaPfBEW2XPZGCaB1rH38tjx21J571JxvkFp3JvSuBpr"
-    case (_, 6) => "BMeiBtFrXuVN7kcVaC4mt1dbncX2n8tb76qUeM4JCr97Cb7U84u"
-    case _ => throw new IllegalArgumentException(s"no scenario defined to get a head hash for branch-$onBranch at time T$atTime")
+  def getHeadHash(onBranch: Int, atLevel: Int) = (onBranch, atLevel) match {
+    case (_, 2) => "BKy8NcuerruFgeCGAoUG3RfjhHf1diYjrgD2qAJ5rNwp2nRJ9H4"
+    case (_, 4) => "BKpFANTnUBqVe8Hm4rUkNuYtJkg7PjLrHjkQaNQj7ph5Bi6qXVi"
+    case (_, 5) => "BLFaY9jHrkuxnQAQv3wJif6V7S6ekGHoxbCBmeuFyLixGAYm3Bp"
+    case (0, 6) => "BMSw4mdnPTRpNtNoRBGpnYDGGH9pcxB2FHBxQeonyUunaV5bAz1"
+    case (2, 6) => "BMKRY5YvFhbwcLPsV3vfvYZ97ktSfu2eJTx2V21PfUxUEYXzTsp"
+    case (_, 7) => "BM7bFA88UaPfBEW2XPZGCaB1rH38tjx21J571JxvkFp3JvSuBpr"
+    case (_, 8) => "BMeiBtFrXuVN7kcVaC4mt1dbncX2n8tb76qUeM4JCr97Cb7U84u"
+    case _ => throw new IllegalArgumentException(s"no scenario defined to get a head hash for branch-$onBranch at time T$atLevel")
   }
 
   /* only a limited number of offsets are expected on the mock calls,
-   * based on the scenario and current time-frame
+   * based on the scenario and current level
    */
-  def getMaxOffsets(onBranch: Int, atTime: Int) = (onBranch, atTime) match {
-    case (_, 1) => 2
-    case (_, 2) => 1
-    case (_, 3) => 1
-    case (0, 4) => 1
-    case (2, 4) => 2
+  def getMaxOffsets(onBranch: Int, atLevel: Int) = (onBranch, atLevel) match {
+    case (_, 2) => 2
+    case (_, 4) => 1
     case (_, 5) => 1
-    case (_, 6) => 3
-    case _ => throw new IllegalArgumentException(s"no scenario defines expected offsets for brach-$onBranch at time T$atTime")
+    case (0, 6) => 1
+    case (2, 6) => 2
+    case (_, 7) => 1
+    case (_, 8) => 3
+    case _ => throw new IllegalArgumentException(s"no scenario defines expected offsets for brach-$onBranch at time T$atLevel")
 
   }
 
@@ -101,14 +110,14 @@ object MockTezosNodes {
     * create a simulated node interface to return pre-canned responses
     * following a known scenario
     */
-  def getNode(onBranch: Int, atTime: Int) = new BaseMock {
+  def getNode(onBranch: Int, atLevel: Int) = new BaseMock {
     import scala.concurrent.ExecutionContext.Implicits.global
 
     //the head should depend on the branch and time both
-    val headHash = getHeadHash(onBranch, atTime)
+    val headHash = getHeadHash(onBranch, atLevel)
 
     //the number of offsets to load should never be higher than this
-    val maxExpectedOffset = getMaxOffsets(onBranch, atTime)
+    val maxExpectedOffset = getMaxOffsets(onBranch, atLevel)
 
     //use some mapping to define valid offsets per scenario?
     def isValidOffset(offset: String) = Try {
@@ -121,11 +130,11 @@ object MockTezosNodes {
       command match {
         case `headRequestUrl` =>
           //will return the block at offset 0
-          getStoredBlock(0, onBranch, atTime)
+          getStoredBlock(0, onBranch, atLevel)
         case HashEndpointMatch(`headHash`, offset) if isValidOffset(offset) =>
-          getStoredBlock(offset.toInt, onBranch, atTime)
+          getStoredBlock(offset.toInt, onBranch, atLevel)
         case HashEndpointMatch(`headHash`, offset) =>
-          throw new IllegalStateException(s"The node simulated for branch-$onBranch at time T$atTime received an unexpected block offset request in $command")
+          throw new IllegalStateException(s"The node simulated for branch-$onBranch at level-$atLevel received an unexpected block offset request in $command")
         case OperationsEndpointMatch(hash) =>
           emptyBlockOperationsResult
         case _ =>
@@ -136,11 +145,11 @@ object MockTezosNodes {
       * Helper function that returns the json block data stored in the forking_tests files.
       * @param offset how many levels away from the current block head
       * @param branch which test chain branch we're working off of
-      * @param time which iteration of lorre we're working off of
+      * @param level  which iteration of lorre we're working off of
       * @return a full json string with the block information
       */
-    private def getStoredBlock(offset: Int, branch: Int, time: Int): Future[String] =
-      Future(scala.io.Source.fromFile(s"src/test/resources/forking_tests/branch-$branch/time-T$time/head~$offset.json").mkString)
+    private def getStoredBlock(offset: Int, branch: Int, level: Int): Future[String] =
+      Future(scala.io.Source.fromFile(s"src/test/resources/forking_tests/branch-$branch/level-$level/head~$offset.json").mkString)
 
   }
 
@@ -154,6 +163,7 @@ object MockTezosNodes {
   class Frame(max: Int) {
 
     private[MockTezosNodes] val cursor = {
+      //a sychronized cell to hold a value, used to simplify a concurrent access to a var
       val synced = new scala.concurrent.SyncVar[Int]()
       synced.put(0)
       synced
@@ -184,7 +194,7 @@ object MockTezosNodes {
 
   /**
     * Creates a sequence of mock nodes, used to simulate
-    * diffent states of the remote node in time
+    * different states of the remote node in time
     * The returned `Frame` is used to move "time" ahead and back by
     * pointing to different nodes in the sequence
     */
@@ -196,6 +206,7 @@ object MockTezosNodes {
   /*
    * A sequence of tezos interfaces that will delegate the get request
    * to the one currently selected by the internal `frame` variable
+   * This way we can emulate the passing of time during the test in a controlled way
    */
   private class NodeSequence(first: TezosNode, rest: TezosNode*) extends BaseMock {
 
@@ -209,33 +220,33 @@ object MockTezosNodes {
   }
 
   //SCENARIO 1 on the scheme
-  lazy val nonForkingScenario = getNode(onBranch = 0, atTime = 1)
+  lazy val nonForkingScenario = getNode(onBranch = 0, atLevel = 2)
 
 
   //SCENARIO 2 on the scheme
   lazy val singleForkScenario = sequenceNodes(
-    getNode(onBranch = 0, atTime = 1),
-    getNode(onBranch = 0, atTime = 2),
-    getNode(onBranch = 1, atTime = 3)
+    getNode(onBranch = 0, atLevel = 2),
+    getNode(onBranch = 0, atLevel = 4),
+    getNode(onBranch = 1, atLevel = 5)
   )
 
   //SCENARIO 3 on the scheme
   lazy val singleForkAlternatingScenario = sequenceNodes(
-    getNode(onBranch = 0, atTime = 1),
-    getNode(onBranch = 0, atTime = 2),
-    getNode(onBranch = 1, atTime = 3),
-    getNode(onBranch = 0, atTime = 4),
-    getNode(onBranch = 1, atTime = 5)
+    getNode(onBranch = 0, atLevel = 2),
+    getNode(onBranch = 0, atLevel = 4),
+    getNode(onBranch = 1, atLevel = 5),
+    getNode(onBranch = 0, atLevel = 6),
+    getNode(onBranch = 1, atLevel = 7)
   )
 
   //SCENARIO 4 on the scheme
   lazy val twoForksAlternatingScenario = sequenceNodes(
-    getNode(onBranch = 0, atTime = 1),
-    getNode(onBranch = 0, atTime = 2),
-    getNode(onBranch = 1, atTime = 3),
-    getNode(onBranch = 2, atTime = 4),
-    getNode(onBranch = 1, atTime = 5),
-    getNode(onBranch = 0, atTime = 6)
+    getNode(onBranch = 0, atLevel = 2),
+    getNode(onBranch = 0, atLevel = 4),
+    getNode(onBranch = 1, atLevel = 5),
+    getNode(onBranch = 2, atLevel = 6),
+    getNode(onBranch = 1, atLevel = 7),
+    getNode(onBranch = 0, atLevel = 8)
   )
 
 }
