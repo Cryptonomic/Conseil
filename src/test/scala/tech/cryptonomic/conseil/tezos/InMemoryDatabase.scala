1b1049e734bde12cc7dca8562d167ab65a60721a
Vishakh <vishakh@users.noreply.github.com>
2018-12-05 19:40:53 -0500

    Changed in memory database timeout.

--- src/test/scala/tech/cryptonomic/conseil/tezos/InMemoryDatabase.scala
+++ src/test/scala/tech/cryptonomic/conseil/tezos/InMemoryDatabase.scala
@@ -78,7 +78,7 @@ trait InMemoryDatabase extends BeforeAndAfterAll with BeforeAndAfterEach {
   }
 
   override protected def beforeEach(): Unit = {
-    Await.ready(dbHandler.run(truncateAll), 30.millis)
+    Await.ready(dbHandler.run(truncateAll), 300.millis)
     super.beforeEach()
   }
 
