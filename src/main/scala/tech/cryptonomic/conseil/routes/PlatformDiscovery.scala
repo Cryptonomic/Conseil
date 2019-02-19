66a14691592bf6d5a4a1f3519bf19e8810ee7cc4
piotrkosecki <piotr.kosecki@gmail.com>
2019-02-19 12:44:01 +0100

    small fixes and refactors

--- src/main/scala/tech/cryptonomic/conseil/routes/PlatformDiscovery.scala
+++ src/main/scala/tech/cryptonomic/conseil/routes/PlatformDiscovery.scala
@@ -13,6 +13,7 @@ import tech.cryptonomic.conseil.tezos.TezosPlatformDiscoveryOperations
 import tech.cryptonomic.conseil.util.ConfigUtil.getNetworks
 import tech.cryptonomic.conseil.util.{ConfigUtil, RouteHandling}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.CachingDirectives._
 
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -32,15 +33,31 @@ object PlatformDiscovery {
 class PlatformDiscovery(config: PlatformsConfiguration, caching: HttpCacheConfiguration)(implicit apiExecutionContext: ExecutionContext)
   extends LazyLogging with RouteHandling with PlatformDiscoveryEndpoints with akkahttp.server.Endpoints with akkahttp.server.JsonSchemaEntities {
 
-  val platformsRoute = platformsEndpoint.implementedBy(_ => ConfigUtil.getPlatforms(config))
-  val networksRoute = networksEndpoint.implementedBy {
+  /** default caching settings */
+  private val defaultCachingSettings: CachingSettings = CachingSettings(caching.cacheConfig)
+
+  /** simple partial function for filtering */
+  private val requestCacheKeyer: PartialFunction[RequestContext, Uri] = {
+    case r: RequestContext => r.request.uri
+  }
+
+  /** LFU caching settings */
+  private val cachingSettings: CachingSettings =
+    defaultCachingSettings.withLfuCacheSettings(defaultCachingSettings.lfuCacheSettings)
+
+  /** LFU cache */
+  private val lfuCache: Cache[Uri, RouteResult] = LfuCache(cachingSettings)
+
+  private val platformsRoute = platformsEndpoint.implementedBy(_ => ConfigUtil.getPlatforms(config))
+
+  private val networksRoute = networksEndpoint.implementedBy {
     case (platform, _) =>
       val networks = getNetworks(config, platform)
-      if (networks.isEmpty)
-        None
+      if (networks.isEmpty) None
       else Some(networks)
   }
-  val entitiesRoute = entitiesEndpoint.implementedByAsync {
+
+  private val entitiesRoute = entitiesEndpoint.implementedByAsync {
     case (platform, network, _) =>
       TezosPlatformDiscoveryOperations.getEntities.map { entities =>
         ConfigUtil.getNetworks(config, platform).find(_.network == network).map { _ =>
@@ -49,7 +66,7 @@ class PlatformDiscovery(config: PlatformsConfiguration, caching: HttpCacheConfig
       }
   }
 
-  val attributesRoute = attributesEndpoint.implementedByAsync {
+  private val attributesRoute = attributesEndpoint.implementedByAsync {
     case ((platform, network, entity), _) =>
       TezosPlatformDiscoveryOperations.getTableAttributes(entity).map { attributes =>
         ConfigUtil.getNetworks(config, platform).find(_.network == network).map { _ =>
@@ -58,7 +75,7 @@ class PlatformDiscovery(config: PlatformsConfiguration, caching: HttpCacheConfig
       }
   }
 
-  val attributesValuesRoute = attributesValuesEndpoint.implementedByAsync {
+  private val attributesValuesRoute = attributesValuesEndpoint.implementedByAsync {
     case ((platform, network, entity), attribute, _) =>
       TezosPlatformDiscoveryOperations.listAttributeValues(entity, attribute).map { attributes =>
         ConfigUtil.getNetworks(config, platform).find(_.network == network).map { _ =>
@@ -67,7 +84,7 @@ class PlatformDiscovery(config: PlatformsConfiguration, caching: HttpCacheConfig
       }
   }
 
-  val attributesValuesWithFilterRoute = attributesValuesWithFilterEndpoint.implementedByAsync {
+  private val attributesValuesWithFilterRoute = attributesValuesWithFilterEndpoint.implementedByAsync {
     case (((platform, network, entity), attribute, filter), _) =>
       TezosPlatformDiscoveryOperations.listAttributeValues(entity, attribute, Some(filter)).map { attributes =>
         ConfigUtil.getNetworks(config, platform).find(_.network == network).map { _ =>
@@ -76,78 +93,14 @@ class PlatformDiscovery(config: PlatformsConfiguration, caching: HttpCacheConfig
       }
   }
 
-  val route: Route = concat(
-    platformsRoute,
-    networksRoute,
-    entitiesRoute,
-    attributesRoute,
-    attributesValuesRoute,
-    attributesValuesWithFilterRoute
-  )
-
-  /** default caching settings */
-  private val defaultCachingSettings: CachingSettings = CachingSettings(caching.cacheConfig)
-  /** simple partial function for filtering */
-  private val requestCacheKeyer: PartialFunction[RequestContext, Uri] = {
-    case r: RequestContext => r.request.uri
+  val route: Route = cache(lfuCache, requestCacheKeyer) {
+    concat(
+      platformsRoute,
+      networksRoute,
+      entitiesRoute,
+      attributesRoute,
+      attributesValuesRoute,
+      attributesValuesWithFilterRoute
+    )
   }
-  /** LFU caching settings */
-  private val cachingSettings: CachingSettings =
-    defaultCachingSettings.withLfuCacheSettings(defaultCachingSettings.lfuCacheSettings)
-  /** LFU cache */
-  private val lfuCache: Cache[Uri, RouteResult] = LfuCache(cachingSettings)
-
-  private def platformNetworkValidation[A](platform: String, network: String)(operation: Future[A]): Future[Option[A]] = {
-    operation.map { xxx =>
-      ConfigUtil.getNetworks(config, platform).find(_.network == network).map { _ =>
-        xxx
-      }
-    }
-  }
-
-  //
-  //  /** Metadata route */
-  //  val route: Route =
-  //    get {
-  //      cache(lfuCache, requestCacheKeyer) {
-  //        pathPrefix("platforms") {
-  //          complete(toJson(ConfigUtil.getPlatforms(config)))
-  //        } ~
-  //          pathPrefix(Segment) { platform =>
-  //            pathPrefix("networks") {
-  //              validatePlatform(config, platform) {
-  //                pathEnd {
-  //                  complete(toJson(ConfigUtil.getNetworks(config, platform)))
-  //                }
-  //              }
-  //            } ~ pathPrefix(Segment) { network =>
-  //              validatePlatformAndNetwork(config, platform, network) {
-  //                pathPrefix("entities") {
-  //                  pathEnd {
-  //                    completeWithJson(TezosPlatformDiscoveryOperations.getEntities)
-  //                  }
-  //                } ~ pathPrefix(Segment) { entity =>
-  //                  validateEntity(entity) {
-  //                    pathPrefix("attributes") {
-  //                      pathEnd {
-  //                        completeWithJson(TezosPlatformDiscoveryOperations.getTableAttributes(entity))
-  //                      }
-  //                    } ~ pathPrefix(Segment) { attribute =>
-  //                      validateAttributes(entity, attribute) {
-  //                        pathEnd {
-  //                          completeWithJson(TezosPlatformDiscoveryOperations.listAttributeValues(entity, attribute))
-  //                        } ~ pathPrefix(Segment) { filter =>
-  //                          pathEnd {
-  //                            completeWithJson(TezosPlatformDiscoveryOperations.listAttributeValues(entity, attribute, Some(filter)))
-  //                          }
-  //                        }
-  //                      }
-  //                    }
-  //                  }
-  //                }
-  //              }
-  //            }
-  //          }
-  //      }
-  //    }
 }
--- src/main/scala/tech/cryptonomic/conseil/routes/openapi/PlatformDiscoveryEndpoints.scala
+++ src/main/scala/tech/cryptonomic/conseil/routes/openapi/PlatformDiscoveryEndpoints.scala
@@ -10,52 +10,58 @@ trait PlatformDiscoveryEndpoints
 
   private val commonPath = path / "v2" / "metadata"
 
-  def platformsEndpoint =
+  def platformsEndpoint: Endpoint[String, List[PlatformDiscoveryTypes.Platform]] =
     endpoint(
       request = get(
         url = commonPath / "platforms" ,
         headers = header("apiKey")),
-      response = jsonResponse[List[PlatformDiscoveryTypes.Platform]](docs = Some("Metadata endpoint for listing available platforms"))
+      response = jsonResponse[List[PlatformDiscoveryTypes.Platform]](docs = Some("Metadata endpoint for listing available platforms")),
+      tags = List("Metadata")
     )
 
-  def networksEndpoint =
+  def networksEndpoint: Endpoint[(String, String), Option[List[PlatformDiscoveryTypes.Network]]] =
     endpoint(
       request = get(
         url = commonPath / segment[String](name = "platforms") / "networks",
         headers = header("apiKey")),
-      response = jsonResponse[List[PlatformDiscoveryTypes.Network]](docs = Some("Metadata endpoint for listing available networks")).orNotFound(Some("Not found"))
+      response = jsonResponse[List[PlatformDiscoveryTypes.Network]](docs = Some("Metadata endpoint for listing available networks")).orNotFound(Some("Not found")),
+      tags = List("Metadata")
     )
 
-  def entitiesEndpoint =
+  def entitiesEndpoint: Endpoint[(String, String, String), Option[List[PlatformDiscoveryTypes.Entity]]] =
     endpoint(
       request = get(
         url = commonPath / segment[String](name = "platform") / segment[String](name = "network") / "entities",
         headers = header("apiKey")),
-      response = jsonResponse[List[PlatformDiscoveryTypes.Entity]](docs = Some("Metadata endpoint for listing available entities")).orNotFound(Some("Not found"))
+      response = jsonResponse[List[PlatformDiscoveryTypes.Entity]](docs = Some("Metadata endpoint for listing available entities")).orNotFound(Some("Not found")),
+      tags = List("Metadata")
     )
 
-  def attributesEndpoint =
+  def attributesEndpoint: Endpoint[((String, String, String), String), Option[List[PlatformDiscoveryTypes.Attribute]]] =
     endpoint(
       request = get(
         url = commonPath / segment[String](name = "platform") / segment[String](name = "network") / segment[String](name = "entity") / "attributes",
         headers = header("apiKey")),
-      response = jsonResponse[List[PlatformDiscoveryTypes.Attribute]](docs = Some("Metadata endpoint for listing available attributes")).orNotFound(Some("Not found"))
+      response = jsonResponse[List[PlatformDiscoveryTypes.Attribute]](docs = Some("Metadata endpoint for listing available attributes")).orNotFound(Some("Not found")),
+      tags = List("Metadata")
     )
 
-  def attributesValuesEndpoint =
+  def attributesValuesEndpoint: Endpoint[((String, String, String), String, String), Option[List[String]]] =
     endpoint(
       request = get(
         url = commonPath / segment[String](name = "platform") / segment[String](name = "network") / segment[String](name = "entity") / segment[String](name = "attribute"),
         headers = header("apiKey")),
-      response = jsonResponse[List[String]](docs = Some("Metadata endpoint for listing available distinct values for given attribute")).orNotFound(Some("Not found"))
+      response = jsonResponse[List[String]](docs = Some("Metadata endpoint for listing available distinct values for given attribute")).orNotFound(Some("Not found")),
+      tags = List("Metadata")
     )
 
-  def attributesValuesWithFilterEndpoint =
+  def attributesValuesWithFilterEndpoint: Endpoint[(((String, String, String), String, String), String), Option[List[String]]] =
     endpoint(
       request = get(
         url = commonPath / segment[String](name = "platform") / segment[String](name = "network") / segment[String](name = "entity") / segment[String](name = "attribute") / segment[String](name = "filter"),
         headers = header("apiKey")),
-      response = jsonResponse[List[String]](docs = Some("Metadata endpoint for listing available distinct values for given attribute filtered")).orNotFound(Some("Not found"))
+      response = jsonResponse[List[String]](docs = Some("Metadata endpoint for listing available distinct values for given attribute filtered")).orNotFound(Some("Not found")),
+      tags = List("Metadata")
     )
 
 }
