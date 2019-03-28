833825d57876295b024a093f6ce64d96ab925c80
Vishakh <vishakh@users.noreply.github.com>
2019-03-28 01:33:28 -0400

    Ensuring desired escaped characters are not filtered out.

--- src/main/scala/tech/cryptonomic/conseil/util/JsonUtil.scala
+++ src/main/scala/tech/cryptonomic/conseil/util/JsonUtil.scala
@@ -56,7 +56,7 @@ object JsonUtil {
     lazy val emptyObject = JsonString("{}")
 
     /** add standard cleaning for input json */
-    def sanitize(s: String): String = s.filterNot(_.isControl).replaceAll("\\\\", "backslash")
+    def sanitize(s: String): String = s.filterNot(_.isControl).replaceAll("""\\u""", "backslashu")
 
   }
 
