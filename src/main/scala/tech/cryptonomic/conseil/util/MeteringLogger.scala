9f3f6e6bb9d8998e73bac1f1e0c61350f224f1bc
ivano.pagano <pagoda_5b@hotmail.com>
2019-07-25 19:33:50 +0200

    Adapt the metering logger to account for typed effects

--- src/main/scala/tech/cryptonomic/conseil/util/MeteringLogger.scala
+++ src/main/scala/tech/cryptonomic/conseil/util/MeteringLogger.scala
@@ -1,22 +1,40 @@
 package tech.cryptonomic.conseil.util
 import scala.concurrent.duration.{Duration, NANOSECONDS}
+import cats.{Monad, Id}
+import cats.implicits._
+import cats.effect.IO
+import scala.concurrent.ExecutionContext
+import cats.effect.Clock
+
+/** Implements metering without wrapping effects, therefore breaking referential transparency */
+class UnsafeMeteringLogger(log: String => Unit) extends MeteringLogger[Id](log) {
+  override def currentMonotoneTime: Long = System.nanoTime()
+}
+
+/** Implements metering using IO */
+class IOMeteringLogger(log: String => IO[Unit])(implicit ec: ExecutionContext) extends MeteringLogger[IO](log) {
+  private val clock = Clock.extractFromTimer(IO.timer(ec))
+
+  override def currentMonotoneTime: IO[Long] = clock.monotonic(NANOSECONDS)
+}
 
 /** Utility class to add time metering of code blocks and logging the details
   * @param log is the function that actually does the logging
   */
-class MeteringLogger(log: String => Unit) {
+  abstract class MeteringLogger[Eff[_] : Monad](log: String => Eff[Unit]) {
+
+  def currentMonotoneTime: Eff[Long]
 
   /** Wraps some code block to log the time taken to execute, in milliseconds
     * @param logDescription used to identify what's being measured in the log prints
     * @param block the code to measure
     * @return the result of execuding the wrapped code
     */
-  def metered[T](logDescription: => String)(block: => T): T = {
-    val start = System.nanoTime()
-    val t = block
-    val lapse = Duration(System.nanoTime() - start, NANOSECONDS).toMillis
-    log(s"$logDescription took $lapse milliseconds")
-    t
-  }
-
+  def metered[T](logDescription: => String)(block: => Eff[T]): Eff[T] = for {
+    start <- currentMonotoneTime
+    result <- block
+    end <- currentMonotoneTime
+    lapse = Duration(end - start, NANOSECONDS).toMillis
+    _ <- log(s"$logDescription took $lapse milliseconds")
+  } yield result
 }
