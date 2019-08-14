e848b5fe4447a15bf9829a9940444647832dd48b
Basil <ba205@users.noreply.github.com>
2019-08-14 20:58:18 +0200

    source is non-null, Ballot is a String in tests

--- src/test/scala/tech/cryptonomic/conseil/tezos/DatabaseConversionsTest.scala
+++ src/test/scala/tech/cryptonomic/conseil/tezos/DatabaseConversionsTest.scala
@@ -838,7 +838,6 @@ class DatabaseConversionsTest
               converted.nonce ::
               converted.pkh ::
               converted.secret ::
-              converted.source ::
               converted.fee ::
               converted.counter ::
               converted.gasLimit ::
