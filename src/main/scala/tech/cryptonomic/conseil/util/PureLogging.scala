d9f855d9fb2b19febb706102642d39ccd8f70dc3
Ivano Pagano <pagoda_5b@hotmail.com>
2019-10-02 12:41:15 +0200

    Guarantee delayed PureLogging

--- src/main/scala/tech/cryptonomic/conseil/util/PureLogging.scala
+++ src/main/scala/tech/cryptonomic/conseil/util/PureLogging.scala
@@ -1,7 +1,7 @@
 package tech.cryptonomic.conseil.util
 
 import com.typesafe.scalalogging.{LazyLogging, Logger}
-import cats._
+import cats.effect.Sync
 
 /** Piggybacks on lightbend logging, adding typed effects to
   * the log commands via extension methods
@@ -11,15 +11,15 @@ trait PureLogging extends LazyLogging {
   /* Add the extensions to the Logger*/
   implicit class PureLoggerOps(base: Logger) {
 
-    /** Wraps the logging operation in a, usually lazy, effect type.
+    /** Wraps the logging operation in a lazy effect type.
       * This means that logging doesn't really happen, until the effect
       * is actually "run"
       * @example {{{
       * logger.pureLog[IO](_.info("my log message"))
       * }}}
       */
-    def pureLog[Eff[_]: Applicative](log: Logger => Unit): Eff[Unit] =
-      Applicative[Eff].pure(log(base))
+    def pureLog[Eff[_]: Sync](log: Logger => Unit): Eff[Unit] =
+      Sync[Eff].delay(log(base))
 
   }
 
