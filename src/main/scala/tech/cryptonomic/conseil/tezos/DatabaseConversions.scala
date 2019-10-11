41cb7cb78b85b93c6f79583711f531c0dc155bbf
Vishakh <vishakh@users.noreply.github.com>
2019-10-11 00:51:14 -0400

    Fixed delegate rendering into database.

--- src/main/scala/tech/cryptonomic/conseil/tezos/DatabaseConversions.scala
+++ src/main/scala/tech/cryptonomic/conseil/tezos/DatabaseConversions.scala
@@ -55,7 +55,7 @@ object DatabaseConversions {
             Tables.AccountsRow(
               accountId = id.id,
               blockId = hash.value,
-              delegate = delegate.flatMap(a => Some(a.toString)),
+              delegate = delegate.flatMap(a => Some(a.value.toString)),
               counter = counter,
               script = script.map(_.code.expression),
               storage = script.map(_.storage.expression),
