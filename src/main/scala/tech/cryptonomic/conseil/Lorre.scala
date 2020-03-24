ed2268d6aa537002ab9f2189b24895fadac4657e
piotrkosecki <piotr.kosecki@gmail.com>
2020-03-24 17:59:50 +0100

    731 - added baker registry table, updated metadata

--- src/main/scala/tech/cryptonomic/conseil/Lorre.scala
+++ src/main/scala/tech/cryptonomic/conseil/Lorre.scala
@@ -1,11 +1,15 @@
 package tech.cryptonomic.conseil
 
+import java.sql.Timestamp
+import java.time.Instant
+
 import akka.actor.ActorSystem
 import akka.Done
 import akka.stream.scaladsl.{Sink, Source}
 import akka.stream.ActorMaterializer
 import mouse.any._
 import com.typesafe.scalalogging.LazyLogging
+import kantan.codecs.strings.StringDecoder
 import org.slf4j.LoggerFactory
 import tech.cryptonomic.conseil.tezos.{
   ApiOperations,
@@ -24,6 +28,7 @@ import tech.cryptonomic.conseil.io.MainOutputs.LorreOutput
 import tech.cryptonomic.conseil.util.DatabaseUtil
 import tech.cryptonomic.conseil.config._
 import tech.cryptonomic.conseil.tezos.TezosNodeOperator.{FetchRights, LazyPages}
+import java.time.format.DateTimeFormatter
 
 import scala.concurrent.duration._
 import scala.annotation.tailrec
@@ -82,8 +87,20 @@ object Lorre extends App with TezosErrors with LazyLogging with LorreAppConfig w
 
   /** Inits tables with values from CSV files */
   import kantan.csv.generic._
+  import kantan.csv._
+
+  implicit val timestampDecoder: CellDecoder[java.sql.Timestamp] = (e: String) => {
+    val format = DateTimeFormatter.ofPattern("EEE MMM dd yyyy HH:mm:ss 'GMT'Z (zzzz)")
+    StringDecoder
+      .makeSafe("Instant")(s => Instant.from(format.parse(s)))(e)
+      .map(Timestamp.from)
+      .left
+      .map(x => DecodeError.TypeError(x.message))
+  }
+
   TezosDb.initTableFromCsv(Tables.RegisteredTokens, tezosConf.network, separator = '|')
   TezosDb.initTableFromCsv(Tables.KnownAddresses, tezosConf.network)
+  TezosDb.initTableFromCsv(Tables.BakerRegistry, tezosConf.network)
 
   /** Schedules method for fetching baking rights */
   system.scheduler.schedule(lorreConf.blockRightsFetching.initDelay, lorreConf.blockRightsFetching.interval)(
