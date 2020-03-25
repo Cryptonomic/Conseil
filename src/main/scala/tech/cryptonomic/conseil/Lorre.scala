2632dfa8ee877a08dc6f8de8faf2a3b2f62b2eef
piotrkosecki <piotr.kosecki@gmail.com>
2020-03-25 18:33:32 +0100

    730 - another small change regarding the type returned

--- src/main/scala/tech/cryptonomic/conseil/Lorre.scala
+++ src/main/scala/tech/cryptonomic/conseil/Lorre.scala
@@ -96,7 +96,7 @@ object Lorre extends App with TezosErrors with LazyLogging with LorreAppConfig w
   def initTableFromCsv[A <: AbstractTable[_], H <: HList](table: TableQuery[A], network: String, separator: Char = ',')(
       implicit hd: HeaderDecoder[A#TableElementType],
       g: Generic.Aux[A#TableElementType, H],
-      m: Mapper.Aux[tech.cryptonomic.conseil.util.ConfigUtil.Csv.Trimmer.type, H, H]
+      m: Mapper.Aux[ConfigUtil.Csv.Trimmer.type, H, H]
   ): Future[Option[Int]] = {
     val rows = ConfigUtil.Csv.readTableRowsFromCsv(table, network, separator)
     db.run(insertWhenEmpty(table, rows)) andThen {
