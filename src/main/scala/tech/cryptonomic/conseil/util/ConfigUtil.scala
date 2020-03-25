0230efb8d0439edde2c1436ec6a3aaaf00996b90
Ivano Pagano <pagoda_5b@hotmail.com>
2020-03-25 15:48:45 +0100

    Add more detailed docs and some test for csv config reading

--- src/main/scala/tech/cryptonomic/conseil/util/ConfigUtil.scala
+++ src/main/scala/tech/cryptonomic/conseil/util/ConfigUtil.scala
@@ -184,7 +184,20 @@ object ConfigUtil {
       implicit def noop[T] = at[T] { identity }
     }
 
-    /** Reads a CSV file with tabular data, returning a set of rows to save in the database */
+    /** Reads a CSV file with tabular data, returning a set of rows to save in the database.
+      * This overload will find the source file based on the slick table and the network provided.
+      *
+      * @tparam T is the slick table for which we want to parse rows objects
+      * @tparam H a generic representation used to automatically map the row from values
+      * @param table used to find which table rows are requested as output
+      * @param network will be used to find the source csv to parse in the search path
+      * @param separator defaults to ',', will be used to split the csv tokens
+      * @param hd implicitly provide a way to read field types from the csv headers
+      * @param g type assertion that a Generic HList can be created from the table row shape
+      * @param m type assertion to state the the HList can be used to map the contents of fields,
+      *          via the polymorphic [[Trimmer]] function
+      * @return the list of parsed rows for the specific table
+      */
     def readTableRowsFromCsv[T <: AbstractTable[_], H <: HList](
         table: TableQuery[T],
         network: String,
@@ -195,21 +208,51 @@ object ConfigUtil {
         g: Generic.Aux[T#TableElementType, H],
         m: Mapper.Aux[Trimmer.type, H, H]
     ): List[T#TableElementType] =
-      readTableRowsFromCsv[T#TableElementType, H](
+      readRowsFromCsv[T#TableElementType, H](
         csvSource = getClass.getResource(s"/${table.baseTableRow.tableName}/$network.csv"),
         separator
       )
 
-    /** Reads a CSV file with tabular data, returning a set of rows to save in the database */
-    def readTableRowsFromCsv[TableRow, H <: HList](
+    /** Reads a CSV file with tabular data, returning a set of rows to save in the database
+      * This overload will use the provided csv file
+      *
+      * To call this method you will need to provide the expected type parameters explicitly,
+      * otherwise the generic method won't be able to fill out the pieces.
+      * To obtain the specific HList type for the [[H]] constraint, we need to pull a "trick",
+      * by using shapeless generic facilities:
+      *
+      * Creating first the [[shapeless.Generic\[Row]]] instance and then accessing its [[Repr]]
+      * type attribute, which will be the specific [[HList]]'s recursive subtype needed to
+      * represent the specific [[Row]] fields.
+      * e.g.
+      * {{{
+      * import shapeless._
+      *
+      * val genericRow = Generic[MyRow]
+      *
+      * val rowList: List[MyRow] = ConfigUtil.Csv.readTableRowsFromCsv[MyRow, genericRow.Repr](csvUrl)
+      *
+      * }}}
+      *
+      * @tparam Row is the type of the parsed objects
+      * @tparam H a generic representation used to automatically map the row from values
+      * @param csvSource points to the csv file to parse
+      * @param separator defaults to ',', will be used to split the csv tokens
+      * @param hd implicitly provide a way to read field types from the csv headers
+      * @param g type assertion that a Generic HList can be created from the table row shape
+      * @param m type assertion to state the the HList can be used to map the contents of fields,
+      *          via the polymorphic [[Trimmer]] function
+      * @return the list of parsed rows
+      */
+    def readRowsFromCsv[Row, H <: HList](
         csvSource: java.net.URL,
-        separator: Char
+        separator: Char = ','
     )(
         implicit
-        hd: HeaderDecoder[TableRow],
-        g: Generic.Aux[TableRow, H],
+        hd: HeaderDecoder[Row],
+        g: Generic.Aux[Row, H],
         m: Mapper.Aux[Trimmer.type, H, H]
-    ): List[TableRow] = {
+    ): List[Row] = {
       import kantan.csv._
       import kantan.csv.ops._
 
@@ -220,11 +263,11 @@ object ConfigUtil {
         g.from(trimmed)
       }
 
-      val reader: CsvReader[ReadResult[TableRow]] =
-        csvSource.asCsvReader[TableRow](rfc.withHeader.withCellSeparator(separator))
+      val reader: CsvReader[ReadResult[Row]] =
+        csvSource.asCsvReader[Row](rfc.withHeader.withCellSeparator(separator))
 
       // separates List[Either[L, R]] into List[L] and List[R]
-      val (errors, rows) = reader.toList.foldRight((List.empty[ReadError], List.empty[TableRow]))(
+      val (errors, rows) = reader.toList.foldRight((List.empty[ReadError], List.empty[Row]))(
         (acc, pair) => acc.fold(l => (l :: pair._1, pair._2), r => (pair._1, trimStringFields(r) :: pair._2))
       )
 
