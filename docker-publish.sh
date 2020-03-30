628eea2fb8501d4a0d0f84651c3a1ddc429a337b
piotrkosecki <piotr.kosecki@gmail.com>
2020-03-30 18:02:20 +0200

    chores - updated docker-compose file and docker publish process

--- docker-publish.sh
+++ docker-publish.sh
@@ -6,19 +6,12 @@ if [ "$1" != "local" ]; then
     [ $? -ne 0 ] && exit
 fi
 
-# needed to generate BuildInfo.scala
-echo "Building Conseil to obtain version"
-sbt clean compile
-
-# gets version from BuildInfo.scala
-DOCKER_TAG=`grep 'val version: String' ./target/scala-2.12/src_managed/main/sbt-buildinfo/BuildInfo.scala |  awk '{print $NF}' | sed -e 's/^"//' -e 's/"$//'`
-
 # builds image
-echo "Building Conseil image with tag $DOCKER_TAG"
-docker build -t cryptonomictech/conseil:$DOCKER_TAG .
+echo "Building Conseil image"
+docker build -t cryptonomictech/conseil .
 
 if [ "$1" != "local" ]; then
 # pushes image to docker hub
-    echo "Publishing Conseil image to Docker Hub with tag $DOCKER_TAG"
-    docker push cryptonomictech/conseil:$DOCKER_TAG
+    echo "Publishing Conseil image to Docker Hub"
+    docker push cryptonomictech/conseil
 fi
