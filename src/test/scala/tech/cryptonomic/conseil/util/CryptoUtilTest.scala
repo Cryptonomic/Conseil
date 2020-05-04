f55be65cef6c0eff529d91986a3f487f4d120423
Ivano Pagano <pagoda_5b@hotmail.com>
2020-05-04 16:38:28 +0200

    Use a function to read hex-account addresses for tokens on big map diffs key (#777)
    * Use an "inverse" function to read hex-account addresses for tokens
    
     * define the code to read addresses as b58check
     * remove the indirect search for matching accounts with the key-hash
     * remove dead code
     * update tests
    
    * Remove unused code due to refactor

--- src/test/scala/tech/cryptonomic/conseil/util/CryptoUtilTest.scala
+++ src/test/scala/tech/cryptonomic/conseil/util/CryptoUtilTest.scala
@@ -4,18 +4,31 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class CryptoUtilTest extends FlatSpec with Matchers {
 
-  "CryptoUtil" should "correctly decode and encode a Tezos account ID" in {
+  "CryptoUtil" should "correctly decode and encode a Tezos account ID as bytes" in {
       val accountID = "tz1Z5pFi5Sy99Kcz36XA5WtKW7Z6NVG9LdA4"
       val decoded = CryptoUtil.base58CheckDecode(accountID, "tz1").get
       val encoded = CryptoUtil.base58CheckEncode(decoded.toList, "tz1").get
       encoded should be(accountID)
     }
 
-  "CryptoUtil" should "correctly decode and encode a Tezos operation ID" in {
+  it should "correctly decode and encode a Tezos operation ID" in {
       val operationID = "op26bhfiE1tVKiZHfkujRcasnghyRnvDx9gnKGiNwAW98M71EWF"
       val decoded = CryptoUtil.base58CheckDecode(operationID, "op").get
       val encoded = CryptoUtil.base58CheckEncode(decoded.toList, "op").get
       encoded should be(operationID)
+    }
+
+  it should "correctly pack and unpack a Tezos account ID as hex-string" in {
+      val accountID = "tz1Z5pFi5Sy99Kcz36XA5WtKW7Z6NVG9LdA4"
+      val packed = CryptoUtil.packAddress(accountID).get
+      //packing adds the packet length at the beginning, read doesn't care
+      val unpacked = CryptoUtil.readAddress(packed.drop(12)).get
+      unpacked should be(accountID)
+    }
+
+  it should "read a binary address to its b58check tezos id" in {
+      val address = CryptoUtil.readAddress("0000a8d45bdc966ddaaac83188a1e1c1fde2a3e05e5c").get
+      address shouldBe "tz1b2icJC4E7Y2ED1xsZXuqYpF7cxHDtduuP"
     }
 
 }
