fbb04820516613c7e69c7a44fa3955378cced0d7
Mariusz Ołownia <mariusz.olownia@scalac.io>
2020-06-16 15:13:06 +0200

    Bitcoin schema with pg arrays

--- conseil-common/src/main/scala/tech/cryptonomic/conseil/common/sql/CustomProfileExtension.scala
+++ conseil-common/src/main/scala/tech/cryptonomic/conseil/common/sql/CustomProfileExtension.scala
@@ -1,14 +1,17 @@
 package tech.cryptonomic.conseil.common.sql
 
-import com.github.tminglei.slickpg.ExPostgresProfile
+import com.github.tminglei.slickpg._
 import slick.basic.Capability
 import slick.jdbc.JdbcCapabilities
 
 /** Custom postgres profile for enabling `insertOrUpdateAll` */
-trait CustomProfileExtension extends ExPostgresProfile {
+trait CustomProfileExtension extends ExPostgresProfile with PgArraySupport {
   // Add back `capabilities.insertOrUpdate` to enable native `upsert` support; for postgres 9.5+
   override protected def computeCapabilities: Set[Capability] =
     super.computeCapabilities + JdbcCapabilities.insertOrUpdate
+
+  override val api = ConseilAPI
+  object ConseilAPI extends API with ArrayImplicits
 }
 
 object CustomProfileExtension extends CustomProfileExtension
