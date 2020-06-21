c618acadc3e89dee1fa8361afaf00b09ce68b2a4
Mariusz Ołownia <mariusz.olownia@scalac.io>
2020-06-21 21:33:28 +0200

    Client documetation

--- conseil-common/src/main/scala/tech/cryptonomic/conseil/common/rpc/RpcClient.scala
+++ conseil-common/src/main/scala/tech/cryptonomic/conseil/common/rpc/RpcClient.scala
@@ -11,7 +11,46 @@ import org.http4s.client.dsl.Http4sClientDsl
 
 import tech.cryptonomic.conseil.common.rpc.RpcClient._
 
-// JSON-RPC client according to specification: https://www.jsonrpc.org/specification
+/**
+  * JSON-RPC client according to the specification at https://www.jsonrpc.org/specification
+  *
+  * Usage example:
+  *
+  * {{{
+  *   import scala.concurrent.ExecutionContext
+  *   import io.circe.generic.auto._
+  *   import org.http4s.circe.CirceEntityDecoder._
+  *   import org.http4s.circe.CirceEntityEncoder._
+  * 
+  *   implicit val contextShift = IO.contextShift(ExecutionContext.global)
+  *   
+  *   // Define case class for JSON-RPC method and response
+  *   case class ApiMethodParams(id: Int)
+  *   case class ApiMethodResponse(name: String)
+  *
+  *   // Create stream with the request
+  *   val request = Stream(
+  *     RpcRequest(
+  *       jsonrpc = "2.0",
+  *       mathod = "apiMethod",
+  *       params = ApiMethodParams(id = 1)
+  *       id = "id_of_the_request"
+  *     )
+  *   )
+  *
+  *   // Create JSON-RPC client
+  *   val client = new RpcClient[IO]("endpoint", 10, httpClient)
+  *
+  *   // Call api method with
+  *   val response: Stream[IO, ApiMethodResponse] =
+  *     client.stream[ApiMethodParams, ApiMethodResponse](batchSize = 10)(request)
+  * }}}
+  *
+  * @param endpoint JSON-RPC server url
+  * @param maxConcurrent Max concurrent processes used to fetch data from JSON-RPC server
+  * @param httpClient Http4s http client
+  * @param headers Additional http headers, e.g. authorization
+  */
 class RpcClient[F[_]: Concurrent](
     endpoint: String,
     maxConcurrent: Int,
@@ -20,6 +59,16 @@ class RpcClient[F[_]: Concurrent](
 ) extends Http4sClientDsl[F]
     with LazyLogging {
 
+  /**
+    * Method that converts [[fs2.Stream]] of JSON-RPC requests into [[fs2.Stream]]] of desired result objects,
+    * or throws the exception with the error. Request [[fs2.Stream]]] can only contain calls to one API method
+    * and can be batched to speed up the response time.
+    *
+    * @param batchSize The size of the batched request in single HTTP call
+    * @param request [[fs2.Stream]]] of the [[RpcRequest]]
+    *
+    * Returns a [[fs2.Stream]]] that contains result from JSON-RPC server call.
+    */
   def stream[P, R](batchSize: Int)(request: Stream[F, RpcRequest[P]])(
       implicit decode: EntityDecoder[F, Seq[RpcResponse[R]]],
       encode: EntityEncoder[F, List[RpcRequest[P]]]
@@ -53,6 +102,14 @@ class RpcClient[F[_]: Concurrent](
 
 object RpcClient {
 
+  /**
+    * Create [[cats.Resource]] with [[RpcClient]].
+    *
+    * @param endpoint JSON-RPC server url
+    * @param maxConcurrent Max concurrent processes used to fetch data from JSON-RPC server
+    * @param httpClient Http4s http client
+    * @param headers Additional http headers, e.g. authorization
+    */
   def resource[F[_]: Concurrent](
       endpoint: String,
       maxConcurrent: Int,
@@ -70,6 +127,23 @@ object RpcClient {
       )
     } yield client
 
+  /**
+    * Exception with JSON-RPC error message
+    * 
+    * Defined Error Codes:
+    * -32700 ---> parse error. not well formed
+    * -32701 ---> parse error. unsupported encoding
+    * -32702 ---> parse error. invalid character for encoding
+    * -32600 ---> server error. invalid xml-rpc. not conforming to spec.
+    * -32601 ---> server error. requested method not found
+    * -32602 ---> server error. invalid method parameters
+    * -32603 ---> server error. internal xml-rpc error
+    * -32500 ---> application error
+    * -32400 ---> system error
+    * -32300 ---> transport error
+    * 
+    * In addition, the range -32099 .. -32000, inclusive is reserved for implementation defined server errors. 
+    */
   case class RpcException(
       code: Int,
       message: String,
