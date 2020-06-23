8b44bce88e0954467a9ee8e4161bd2894082c28f
Mariusz Ołownia <mariusz.olownia@scalac.io>
2020-06-23 14:29:48 +0200

    Remarks form the PR

--- conseil-common/src/test/scala/tech/cryptonomic/conseil/common/bitcoin/rpc/BitcoinRpcClientTest.scala
+++ conseil-common/src/test/scala/tech/cryptonomic/conseil/common/bitcoin/rpc/BitcoinRpcClientTest.scala
@@ -17,14 +17,14 @@ class BitcoinRpcClientTest extends WordSpec with Matchers with BitcoinFixtures {
 
   "Bitcoin JSON-RPC client" should {
 
-      "return a blockchain info" in new BitcoinClientFixtures {
+      "return a blockchain info" in new BitcoinClientStubs {
         bitcoinClientStub(JsonFixtures.getBlockchainInfoResponse).getBlockChainInfo.compile.toList
           .unsafeRunSync() shouldBe List(
               RpcFixtures.blockchainInfoResult
             )
       }
 
-      "return a block hash for the given height" in new BitcoinClientFixtures {
+      "return a block hash for the given height" in new BitcoinClientStubs {
         Stream(102000)
           .through(bitcoinClientStub(JsonFixtures.getBlockHashResponse).getBlockHash(batchSize = 1))
           .compile
@@ -32,7 +32,7 @@ class BitcoinRpcClientTest extends WordSpec with Matchers with BitcoinFixtures {
           .unsafeRunSync() shouldBe List("00000000000335c47dd6ae953912d172a4d9839355f2083165043bb6f43c2f58")
       }
 
-      "return a block for the given hash" in new BitcoinClientFixtures {
+      "return a block for the given hash" in new BitcoinClientStubs {
         Stream("00000000000335c47dd6ae953912d172a4d9839355f2083165043bb6f43c2f58")
           .through(bitcoinClientStub(JsonFixtures.getBlockResponse).getBlockByHash(batchSize = 1))
           .compile
@@ -40,7 +40,7 @@ class BitcoinRpcClientTest extends WordSpec with Matchers with BitcoinFixtures {
           .unsafeRunSync() shouldBe List(RpcFixtures.blockResult)
       }
 
-      "return a transactions (with imputs and outputs) for the given block" in new BitcoinClientFixtures {
+      "return a transactions (with imputs and outputs) for the given block" in new BitcoinClientStubs {
         Stream(RpcFixtures.blockResult)
           .through(bitcoinClientStub(JsonFixtures.getRawTransactionResponse).getBlockWithTransactions(batchSize = 1))
           .compile
