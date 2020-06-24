d0d5a0d69afe5e6c2841d8d520c142e72007b466
Mariusz Ołownia <mariusz.olownia@scalac.io>
2020-06-24 13:48:00 +0200

    Sealed trait in rpc methods and responses

--- conseil-common/src/main/scala/tech/cryptonomic/conseil/common/bitcoin/rpc/BitcoinRpcCommands.scala
+++ conseil-common/src/main/scala/tech/cryptonomic/conseil/common/bitcoin/rpc/BitcoinRpcCommands.scala
@@ -11,11 +11,16 @@ import tech.cryptonomic.conseil.common.rpc.RpcClient.RpcRequest
 object BitcoinRpcCommands {
 
   /**
+    * Sealed trait to keep the list of the RPC methods only in this file.
+    */
+  sealed trait BitcoinRpcMethod
+
+  /**
     * `getblockchaininfo` Bitcoin JSON-RPC api method.
     * Returns an object containing various state info regarding blockchain processing
     * https://developer.bitcoin.org/reference/rpc/getblockchaininfo.html
     */
-  object GetBlockChainInfo {
+  object GetBlockChainInfo extends BitcoinRpcMethod {
     val rpcMethod = "getblockchaininfo"
     def request = RpcRequest("1.0", rpcMethod, Nil, "gbci")
   }
@@ -25,7 +30,7 @@ object BitcoinRpcCommands {
     * Returns hash of block in best-block-chain at height provided.
     * https://developer.bitcoin.org/reference/rpc/getblockhash.html
     */
-  object GetBlockHash {
+  object GetBlockHash extends BitcoinRpcMethod {
     val rpcMethod = "getblockhash"
     case class Params(height: Int)
     def request(height: Int) = RpcRequest("1.0", rpcMethod, Params(height), s"gbh_$height")
@@ -38,7 +43,7 @@ object BitcoinRpcCommands {
     * If verbosity is 2, returns an Object with information about block ‘hash’ and information about each transaction.
     * https://developer.bitcoin.org/reference/rpc/getblock.html
     */
-  object GetBlock {
+  object GetBlock extends BitcoinRpcMethod {
     val rpcMethod = "getblock"
     case class Params(hash: String, verbosity: Int)
     def request(hash: String) = RpcRequest("1.0", rpcMethod, Params(hash, 1), s"gb_$hash")
@@ -57,7 +62,7 @@ object BitcoinRpcCommands {
     * If verbose is ‘true’, returns an Object with information about ‘txid’.
     * https://developer.bitcoin.org/reference/rpc/getrawtransaction.html
     */
-  object GetRawTransaction {
+  object GetRawTransaction extends BitcoinRpcMethod {
     val rpcMethod = "getrawtransaction"
     case class Params(txid: String, verbose: Boolean)
     def request(txid: String) = RpcRequest("1.0", rpcMethod, Params(txid, true), s"grt_$txid")
