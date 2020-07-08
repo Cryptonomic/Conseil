7a7474dad1607a08947e979450b7a01afc77e80c
Mariusz Ołownia <mariusz.olownia@scalac.io>
2020-07-08 10:29:33 +0200

    BitcoinOperationsTest refactor

--- conseil-lorre/src/test/scala/tech/cryptonomic/conseil/indexer/bitcoin/BitcoinOperationsTest.scala
+++ conseil-lorre/src/test/scala/tech/cryptonomic/conseil/indexer/bitcoin/BitcoinOperationsTest.scala
@@ -6,32 +6,27 @@ import cats.effect.{ContextShift, IO}
 import fs2.Stream
 import slickeffect.Transactor
 import org.scalatest.{Matchers, WordSpec}
-import org.scalatest.Matchers
 import org.scalamock.scalatest.MockFactory
 
-import tech.cryptonomic.conseil.indexer.config.{Custom, Everything, Newest}
 import tech.cryptonomic.conseil.common.config.Platforms.BitcoinBatchFetchConfiguration
 import tech.cryptonomic.conseil.common.rpc.RpcClient
-import tech.cryptonomic.conseil.common.bitcoin.Tables
-import tech.cryptonomic.conseil.common.bitcoin.{BitcoinFixtures, BitcoinInMemoryDatabaseSetup, BitcoinStubs}
+import tech.cryptonomic.conseil.common.bitcoin.{BitcoinFixtures, BitcoinPersistence, Tables}
 import tech.cryptonomic.conseil.common.bitcoin.rpc.BitcoinClient
 import tech.cryptonomic.conseil.common.bitcoin.rpc.json.BlockchainInfo
-import tech.cryptonomic.conseil.common.bitcoin.BitcoinPersistence
-import tech.cryptonomic.conseil.common.testkit.InMemoryDatabase
+import tech.cryptonomic.conseil.indexer.config.{Custom, Everything, Newest}
 
-class BitcoinOperationsTest
-    extends WordSpec
-    with MockFactory
-    with Matchers
-    with InMemoryDatabase
-    with BitcoinInMemoryDatabaseSetup
-    with BitcoinFixtures
-    with BitcoinStubs {
+class BitcoinOperationsTest extends WordSpec with MockFactory with Matchers with BitcoinFixtures {
 
   implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
 
   "Bitcoin operations" should {
       "run indexer for the newest blocks" in new BitcoinOperationsStubs {
+        // Bitcoin operations with mocked loadBlocksWithTransactions to test only loadBlocks.
+        val bitcoinOperationsStub = new BitcoinOperationsMock {
+          override def loadBlocksWithTransactions(range: Range.Inclusive) =
+            bitcoinOperationsMock.loadBlocksWithTransactions(range)
+        }
+
         // check if the method gets the latest block at the beginning
         (bitcoinPersistenceMock.getLatestIndexedBlock _) expects ()
         (txMock.transact[Option[Tables.BlocksRow]] _) expects (*) returning (IO(
@@ -42,12 +37,19 @@ class BitcoinOperationsTest
         (bitcoinClientMock.getBlockChainInfo _) expects () returning (Stream(BlockchainInfo("mainnet", 10)).covary[IO])
 
         // test if method runs indexer with the correct range of the blocks
-        (dummyBitcoinOperationsMock.loadBlocksWithTransactions _) expects (6 to 10) returning (Stream.empty)
+        (bitcoinOperationsMock.loadBlocksWithTransactions _) expects (6 to 10) returning (Stream.empty)
 
-        dummyBitcoinOperationsStub.loadBlocks(Newest).compile.drain.unsafeRunSync()
+        // run
+        bitcoinOperationsStub.loadBlocks(Newest).compile.drain.unsafeRunSync()
       }
 
       "run indexer for the all blocks" in new BitcoinOperationsStubs {
+        // Bitcoin operations with mocked loadBlocksWithTransactions to test only loadBlocks.
+        val bitcoinOperationsStub = new BitcoinOperationsMock {
+          override def loadBlocksWithTransactions(range: Range.Inclusive) =
+            bitcoinOperationsMock.loadBlocksWithTransactions(range)
+        }
+
         // check if the method gets the latest block at the beginning
         (bitcoinPersistenceMock.getLatestIndexedBlock _) expects ()
         (txMock.transact[Option[Tables.BlocksRow]] _) expects (*) returning (IO(None))
@@ -56,12 +58,19 @@ class BitcoinOperationsTest
         (bitcoinClientMock.getBlockChainInfo _) expects () returning (Stream(BlockchainInfo("mainnet", 10)).covary[IO])
 
         // test if method runs indexer with the correct range of the blocks
-        (dummyBitcoinOperationsMock.loadBlocksWithTransactions _) expects (1 to 10) returning (Stream.empty)
+        (bitcoinOperationsMock.loadBlocksWithTransactions _) expects (1 to 10) returning (Stream.empty)
 
-        dummyBitcoinOperationsStub.loadBlocks(Everything).compile.drain.unsafeRunSync()
+        // run
+        bitcoinOperationsStub.loadBlocks(Everything).compile.drain.unsafeRunSync()
       }
 
       "run indexer for the custom blocks range" in new BitcoinOperationsStubs {
+        // Bitcoin operations with mocked loadBlocksWithTransactions to test only loadBlocks.
+        val bitcoinOperationsStub = new BitcoinOperationsMock {
+          override def loadBlocksWithTransactions(range: Range.Inclusive) =
+            bitcoinOperationsMock.loadBlocksWithTransactions(range)
+        }
+
         // check if the method gets the latest block at the beginning
         (bitcoinPersistenceMock.getLatestIndexedBlock _) expects ()
         (txMock.transact[Option[Tables.BlocksRow]] _) expects (*) returning (IO(
@@ -72,28 +81,10 @@ class BitcoinOperationsTest
         (bitcoinClientMock.getBlockChainInfo _) expects () returning (Stream(BlockchainInfo("mainnet", 10)).covary[IO])
 
         // test if method runs indexer with the correct range of the blocks
-        (dummyBitcoinOperationsMock.loadBlocksWithTransactions _) expects (7 to 10) returning (Stream.empty)
+        (bitcoinOperationsMock.loadBlocksWithTransactions _) expects (7 to 10) returning (Stream.empty)
 
-        dummyBitcoinOperationsStub.loadBlocks(Custom(3)).compile.drain.unsafeRunSync()
-      }
-
-      "index blocks with transactions with the given range" in new BitcoinOperationsStubs {
-        // check if the method gets existing blocks at the beginning
-        (bitcoinPersistenceMock.getIndexedBlockHeights _) expects (1 to 10)
-        (txMock.transact[Seq[Int]] _) expects (*) returning (IO((1 to 10).toVector))
-
-        // mock Bitcoin client calls
-        (bitcoinClientMock.getBlockHash _) expects (1) returning (_ => Stream("hash").covary[IO])
-        (bitcoinClientMock.getBlockByHash _) expects (1) returning (_ => Stream(RpcFixtures.blockResult).covary[IO])
-        (bitcoinClientMock.getBlockWithTransactions _) expects (1) returning (
-                _ => Stream((RpcFixtures.blockResult, List(RpcFixtures.transactionResult))).covary[IO]
-            )
-
-        // check if the method saves data to the database
-        (bitcoinPersistenceMock.createBlock _) expects (*, *)
-        (txMock.transact[Unit] _) expects (*) returning (IO.unit)
-
-        bitcoinOperationsStub.loadBlocksWithTransactions(1 to 10).compile.drain.unsafeRunSync()
+        // run
+        bitcoinOperationsStub.loadBlocks(Custom(3)).compile.drain.unsafeRunSync()
       }
     }
 
@@ -125,36 +116,10 @@ class BitcoinOperationsTest
     val bitcoinPersistenceMock = mock[BitcoinPersistenceMock]
     val txMock = mock[Transactor[IO]]
 
-    class DummyBitcoinOperationsMock
+    class BitcoinOperationsMock
         extends BitcoinOperations[IO](bitcoinClientMock, bitcoinPersistenceMock, txMock, batchConfig)
 
-    val dummyBitcoinOperationsMock = mock[DummyBitcoinOperationsMock]
-
-    /**
-      * BitcoinOperations with mocked: Bitcoin client, Persistance and transactor.
-      */
-    def bitcoinOperationsStub: BitcoinOperations[IO] =
-      new BitcoinOperations(
-        bitcoinClient = bitcoinClientMock,
-        persistence = bitcoinPersistenceMock,
-        tx = txMock,
-        batchConf = batchConfig
-      )
-
-    /**
-      * BitcoinOperations with mocked `loadBlocksWithTransactions`.
-      * The method is used when testing the indexer startup with Depth.
-      */
-    def dummyBitcoinOperationsStub: BitcoinOperations[IO] =
-      new BitcoinOperations(
-        bitcoinClient = bitcoinClientMock,
-        persistence = bitcoinPersistenceMock,
-        tx = txMock,
-        batchConf = batchConfig
-      ) {
-        override def loadBlocksWithTransactions(range: Range.Inclusive) =
-          dummyBitcoinOperationsMock.loadBlocksWithTransactions(range)
-      }
+    val bitcoinOperationsMock = mock[BitcoinOperationsMock]
   }
 
 }
