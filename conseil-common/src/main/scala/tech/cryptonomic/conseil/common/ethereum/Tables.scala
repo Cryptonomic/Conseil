3aaf09d5c8462d6dfdbc7208aaed7e9aa2e8cc6a
Piotr Bandurski <piotr.bandurski@scalac.io>
2021-05-11 12:06:10 +0200

    regenerate Tables.scala for ethereum

--- conseil-common/src/main/scala/tech/cryptonomic/conseil/common/ethereum/Tables.scala
+++ conseil-common/src/main/scala/tech/cryptonomic/conseil/common/ethereum/Tables.scala
@@ -1,4 +1,5 @@
 package tech.cryptonomic.conseil.common.ethereum
+
 // AUTO-GENERATED Slick data model
 /** Stand-alone Slick data model for immediate use */
 object Tables extends {
@@ -61,7 +62,9 @@ trait Tables {
       e1: GR[Int],
       e2: GR[Option[java.sql.Timestamp]],
       e3: GR[scala.math.BigDecimal],
-      e4: GR[Option[String]]
+      e4: GR[Option[String]],
+      e5: GR[Option[Int]],
+      e6: GR[Option[scala.math.BigDecimal]]
   ): GR[AccountsRow] = GR { prs =>
     import prs._
     AccountsRow.tupled(
@@ -161,6 +164,9 @@ trait Tables {
     /** Database column total_supply SqlType(numeric), Default(None) */
     val totalSupply: Rep[Option[scala.math.BigDecimal]] =
       column[Option[scala.math.BigDecimal]]("total_supply", O.Default(None))
+
+    /** Index over (tokenStandard) (database name ix_accounts_token_standard) */
+    val index1 = index("ix_accounts_token_standard", tokenStandard)
   }
 
   /** Collection-like TableQuery object for table Accounts */
@@ -225,6 +231,9 @@ trait Tables {
 
     /** Index over (address) (database name ix_accounts_history_address) */
     val index1 = index("ix_accounts_history_address", address)
+
+    /** Index over (blockNumber) (database name ix_accounts_history_block_number) */
+    val index2 = index("ix_accounts_history_block_number", blockNumber)
   }
 
   /** Collection-like TableQuery object for table AccountsHistory */
@@ -444,6 +453,9 @@ trait Tables {
 
     /** Database column timestamp SqlType(timestamp) */
     val timestamp: Rep[java.sql.Timestamp] = column[java.sql.Timestamp]("timestamp")
+
+    /** Index over (level) (database name ix_blocks_level) */
+    val index1 = index("ix_blocks_level", level)
   }
 
   /** Collection-like TableQuery object for table Blocks */
@@ -773,11 +785,14 @@ trait Tables {
     /** Database column asof SqlType(timestamp) */
     val asof: Rep[java.sql.Timestamp] = column[java.sql.Timestamp]("asof")
 
-    /** Index over (accountAddress) (database name ix_account_address) */
-    val index1 = index("ix_account_address", accountAddress)
+    /** Index over (accountAddress) (database name ix_tokens_history_account_address) */
+    val index1 = index("ix_tokens_history_account_address", accountAddress)
 
-    /** Index over (tokenAddress) (database name ix_token_address) */
-    val index2 = index("ix_token_address", tokenAddress)
+    /** Index over (blockNumber) (database name ix_tokens_history_block_number) */
+    val index2 = index("ix_tokens_history_block_number", blockNumber)
+
+    /** Index over (tokenAddress) (database name ix_tokens_history_token_address) */
+    val index3 = index("ix_tokens_history_token_address", tokenAddress)
   }
 
   /** Collection-like TableQuery object for table TokensHistory */
@@ -882,6 +897,18 @@ trait Tables {
 
     /** Database column value SqlType(numeric) */
     val value: Rep[scala.math.BigDecimal] = column[scala.math.BigDecimal]("value")
+
+    /** Index over (tokenAddress) (database name ix_token_transfers_address) */
+    val index1 = index("ix_token_transfers_address", tokenAddress)
+
+    /** Index over (blockNumber) (database name ix_token_transfers_block_number) */
+    val index2 = index("ix_token_transfers_block_number", blockNumber)
+
+    /** Index over (fromAddress) (database name ix_token_transfers_from) */
+    val index3 = index("ix_token_transfers_from", fromAddress)
+
+    /** Index over (toAddress) (database name ix_token_transfers_to) */
+    val index4 = index("ix_token_transfers_to", toAddress)
   }
 
   /** Collection-like TableQuery object for table TokenTransfers */
@@ -1073,6 +1100,15 @@ trait Tables {
       onUpdate = ForeignKeyAction.NoAction,
       onDelete = ForeignKeyAction.NoAction
     )
+
+    /** Index over (blockNumber) (database name ix_transactions_block_number) */
+    val index1 = index("ix_transactions_block_number", blockNumber)
+
+    /** Index over (destination) (database name ix_transactions_destination) */
+    val index2 = index("ix_transactions_destination", destination)
+
+    /** Index over (source) (database name ix_transactions_source) */
+    val index3 = index("ix_transactions_source", source)
   }
 
   /** Collection-like TableQuery object for table Transactions */
