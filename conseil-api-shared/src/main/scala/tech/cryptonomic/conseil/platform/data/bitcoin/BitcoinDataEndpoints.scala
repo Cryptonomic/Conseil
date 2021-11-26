abe5bdd323b2b2e6970d02a592af4069e029a28e
amillert <devmood.business@gmail.com>
2021-11-26 12:35:25 +0100

    wip btc data endpoints

---
+++ conseil-api-shared/src/main/scala/tech/cryptonomic/conseil/platform/data/bitcoin/BitcoinDataEndpoints.scala
@@ -0,0 +1,81 @@
+package tech.cryptonomic.conseil.api.routes.platform.data.bitcoin
+
+import sttp.tapir._
+import tech.cryptonomic.conseil.platform.data._
+import tech.cryptonomic.conseil.common.generic.chain.DataTypes.QueryResponse
+
+/** Trait containing endpoints definition */
+trait BitcoinDataEndpoints extends ApiDataEndpoints { // with BitcoinFilterFromQueryString {
+
+  private val platform = "bitcoin"
+
+  private val root = "v2" / "data" / platform / query[String]("network")
+
+  def bitcoinQueryEndpoint = queryEndpoint(platform)
+
+  /** V2 Blocks endpoint definition */
+  def bitcoinBlocksEndpoint =
+    infallibleEndpoint.get
+      .in(root / "blocks" / query[Option[String]]("bitcoinQsFilter"))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[List[QueryResponse]]("blocks"))
+
+  /** V2 Blocks head endpoint definition */
+  def bitcoinBlocksHeadEndpoint =
+    infallibleEndpoint.get
+      .in(root / "blocks" / "head")
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[QueryResponse]("blocks head"))
+
+  /** V2 Blocks by hash endpoint definition */
+  def bitcoinBlockByHashEndpoint =
+    infallibleEndpoint.get
+      .in(root / "blocks" / query[String]("hash"))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[QueryResponse]("block by hash"))
+
+  /** V2 Transactions endpoint definition */
+  def bitcoinTransactionsEndpoint =
+    infallibleEndpoint.get
+      .in(root / "transactions" / query[Option[String]](bitcoinQsFilter))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[List[QueryResponse]]("transactions"))
+
+  /** V2 Transaction by id endpoint definition */
+  def bitcoinTransactionByIdEndpoint =
+    infallibleEndpoint.get
+      .in(root / "transactions" / query[String]("id"))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[QueryResponse]("transaction by id"))
+
+  /** V2 Inputs for transactions endpoint definition */
+  def bitcoinInputsEndpoint =
+    infallibleEndpoint.get
+      .in(root / "inputs" / query[Option[String]](bitcoinQsFilter))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[List[QueryResponse]]("inputs for transactions"))
+
+  /** V2 Outputs for transactions endpoint definition */
+  def bitcoinOutputsEndpoint =
+    infallibleEndpoint.get
+      .in(root / "outputs" / query[Option[String]](bitcoinQsFilter))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[List[QueryResponse]]("outputs for transactions"))
+
+  /** V2 Accounts endpoint definition */
+  def bitcoinAccountsEndpoint =
+    infallibleEndpoint.get
+      .in(root / "accounts" / query[Option[String]](bitcoinQsFilter))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[List[QueryResponse]]("accounts"))
+
+  /** V2 Accounts by address endpoint definition */
+  def bitcoinAccountByAddressEndpoint =
+    infallibleEndpoint.get
+      .in(root / "accounts" / query[String]("address"))
+      .in(header[Option[String]]("apiKey"))
+      .out(compatibilityQuery[QueryResponse]("account by address"))
+
+  private def createTags(entity: String): List[String] = List(s"Bitcoin $entity")
+
+}
