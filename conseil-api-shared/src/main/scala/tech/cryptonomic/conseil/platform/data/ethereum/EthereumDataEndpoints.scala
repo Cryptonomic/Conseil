95690a5957220edd590b25c7bde46e679f0ac47f
amillert <devmood.business@gmail.com>
2021-11-26 12:03:23 +0100

    wip eth data endpoints

---
+++ conseil-api-shared/src/main/scala/tech/cryptonomic/conseil/platform/data/ethereum/EthereumDataEndpoints.scala
@@ -0,0 +1,70 @@
+package tech.cryptonomic.conseil.platform.data.ethereum
+
+/** Represents list of endpoints exposed for Ethereum Blockchain */
+trait EthereumDataEndpoints extends EthereumDataEndpointsCreator {
+
+  private val platform: String = "ethereum"
+
+  def ethereumQueryEndpoint = queryEndpoint(platform)
+
+  def ethereumBlocksEndpoint = blocksEndpoint(platform)
+
+  def ethereumBlocksHeadEndpoint = blocksHeadEndpoint(platform)
+
+  def ethereumBlockByHashEndpoint = blockByHashEndpoint(platform)
+
+  def ethereumTransactionsEndpoint = transactionsEndpoint(platform)
+
+  def ethereumTransactionByHashEndpoint = transactionByHashEndpoint(platform)
+
+  def ethereumLogsEndpoint = logsEndpoint(platform)
+
+  def ethereumReceiptsEndpoint = receiptsEndpoint(platform)
+
+  def ethereumContractsEndpoint = contractsEndpoint(platform)
+
+  def ethereumTokensEndpoint = tokensEndpoint(platform)
+
+  def ethereumTokenTransfersEndpoint = tokenTransfersEndpoint(platform)
+
+  def ethereumTokensHistoryEndpoint = tokensHistoryEndpoint(platform)
+
+  def ethereumAccountsEndpoint = accountsEndpoint(platform)
+
+  def ethereumAccountByAddressEndpoint = accountByAddressEndpoint(platform)
+
+  def ethereumAccountsHistoryEndpoint = accountsHistoryEndpoint(platform)
+}
+
+/** Represents list of endpoints exposed for Quorum Blockchain (based on Ethereum) */
+trait QuorumDataEndpoints extends EthereumDataEndpointsCreator {
+
+  private val platform: String = "quorum"
+
+  def quorumQueryEndpoint = queryEndpoint(platform)
+
+  def quorumBlocksEndpoint = blocksEndpoint(platform)
+
+  def quorumBlocksHeadEndpoint = blocksHeadEndpoint(platform)
+
+  def quorumBlockByHashEndpoint = blockByHashEndpoint(platform)
+
+  def quorumTransactionsEndpoint = transactionsEndpoint(platform)
+
+  def quorumTransactionByHashEndpoint = transactionByHashEndpoint(platform)
+
+  def quorumLogsEndpoint = logsEndpoint(platform)
+
+  def quorumReceiptsEndpoint = receiptsEndpoint(platform)
+
+  def quorumContractsEndpoint = contractsEndpoint(platform)
+
+  def quorumTokensEndpoint = tokensEndpoint(platform)
+
+  def quorumTokenTransfersEndpoint = tokenTransfersEndpoint(platform)
+
+  def quorumAccountsEndpoint = accountsEndpoint(platform)
+
+  def quorumAccountByAddressEndpoint = accountByAddressEndpoint(platform)
+
+}
