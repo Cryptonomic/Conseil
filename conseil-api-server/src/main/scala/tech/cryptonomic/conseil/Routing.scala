768f1fcc1cf268d2063fc50239368e008b913179
amillert <devmood.business@gmail.com>
2021-12-14 15:42:57 +0100

    Add initial functionality for open api docs generation

--- conseil-api-server/src/main/scala/tech/cryptonomic/conseil/Routing.scala
+++ conseil-api-server/src/main/scala/tech/cryptonomic/conseil/Routing.scala
@@ -3,22 +3,32 @@ package tech.cryptonomic.conseil
 import cats.effect.{Async, IO}
 import org.http4s.HttpApp
 import sttp.model.StatusCode
+import sttp.tapir.docs.openapi.OpenAPIDocsInterpreter
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe._
+import sttp.tapir.openapi.OpenAPI
+import sttp.tapir.openapi.circe.yaml._
+import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.http4s.{Http4sServerInterpreter, Http4sServerOptions}
 import sttp.tapir.server.interceptor.ValuedEndpointOutput
 import sttp.tapir.statusCode
+import sttp.tapir.swagger.SwaggerUI
 
-object Routing {
+trait APIDocs {
+  val docs: OpenAPI = OpenAPIDocsInterpreter().toOpenAPI(protocol.appInfo, "My App", "1.0")
+  val docsRoute: List[ServerEndpoint[Any, IO]] = SwaggerUI[IO](docs.toYaml)
+}
+
+object Routing extends APIDocs {
 
   import tech.cryptonomic.conseil.info.model._
   import tech.cryptonomic.conseil.info.converters._
 
-  def instance[F[_]: Async]: HttpApp[IO] = {
+  val pai: ServerEndpoint[Any, IO] = protocol.appInfo.serverLogicSuccess(_ => emptyInfo)
 
-    val endpoints =
-      List(protocol.appInfo.serverLogicSuccess(_ => emptyInfo))
+  val endpoints = pai :: docsRoute
 
+  def instance[F[_]: Async]: HttpApp[IO] =
     Http4sServerInterpreter[IO](
       Http4sServerOptions
         .customInterceptors[IO, IO]
@@ -32,6 +42,5 @@ object Routing {
         }
         .options
     ).toRoutes(endpoints).orNotFound
-  }
 
 }
