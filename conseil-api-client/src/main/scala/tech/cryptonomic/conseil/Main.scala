a5d3aab87fbbf01d95ccdef2ff1f399c50adcbdd
amillert <devmood.business@gmail.com>
2021-12-15 18:46:01 +0100

    Minor update in client main

--- conseil-api-client/src/main/scala/tech/cryptonomic/conseil/Main.scala
+++ conseil-api-client/src/main/scala/tech/cryptonomic/conseil/Main.scala
@@ -11,15 +11,16 @@ import tech.cryptonomic.conseil.info.model.GenericServerError
 import tech.cryptonomic.conseil.info.converters.genericServerErrorDecoder
 
 object Main extends IOApp.Simple {
+  val localhost = Some(uri"http://localhost:8080")
+  val clientInterpretter = Http4sClientInterpreter[IO]()
+
   def run: IO[Unit] =
     EmberClientBuilder
       .default[IO]
       .build
       .use { client =>
         val (req, handler) =
-          Http4sClientInterpreter[IO]()
-            .toRequestUnsafe(protocol.appInfo, Some(uri"http://localhost:8080"))
-            .apply(None, "api key")
+          clientInterpretter.toRequestUnsafe(protocol.appInfo, localhost).apply("api key")
 
         client
           .run(req)
@@ -32,4 +33,17 @@ object Main extends IOApp.Simple {
           }
           .void
       }
+
+  // import tech.cryptonomic.conseil.platform.data.tezos.TezosDataEndpoints._
+  // val (r, h) =
+  //   clientInterpretter.toRequestUnsafe(tezosQueryEndpoint, localhost).apply(()) //"api key")
+  // // val (rr, hh) =
+  // // clientInterpretter.toRequestUnsafe(tezosBlocksEndpoint, localhost).apply()
+  // val (rrr, hhh) =
+  //   clientInterpretter.toRequestUnsafe(tezosBlocksHeadEndpoint, localhost).apply(()) // None, "api key")
+  // val (rrrr, hhhh) =
+  //   clientInterpretter.toRequestUnsafe(tezosBlockByHashEndpoint, localhost).apply("") // , "api key")
+  // // val (rrrrr, hhhhh) =
+  // //   clientInterpretter.toRequestUnsafe(tezosAccountsEndpoint, localhost).apply(None, "api key")
+
 }
