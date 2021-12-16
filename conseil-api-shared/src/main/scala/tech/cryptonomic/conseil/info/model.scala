1351cd21333d1cf6a6cd457095000005a7103d80
amillert <devmood.business@gmail.com>
2021-12-16 08:59:21 +0100

    Updated app info with real git info from build

--- conseil-api-shared/src/main/scala/tech/cryptonomic/conseil/info/model.scala
+++ conseil-api-shared/src/main/scala/tech/cryptonomic/conseil/info/model.scala
@@ -2,11 +2,14 @@ package tech.cryptonomic.conseil.info
 
 import cats.effect.IO
 
+import tech.cryptonomic.conseil.BuildInfo
+
 object model {
 
   final case class Info(application: String, version: String, git: GitInfo)
 
-  val emptyInfo = IO(Info("", "", GitInfo(None, List.empty[String])))
+  val emptyInfo =
+    IO(Info(BuildInfo.name, BuildInfo.version, GitInfo(BuildInfo.gitHeadCommit, BuildInfo.gitCurrentTags.toList)))
 
   final case class GitInfo(commitHash: Option[String], tags: List[String])
 
