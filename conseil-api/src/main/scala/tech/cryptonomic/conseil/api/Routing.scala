379960d8ebfad1024546c1c880e398d4d2e0d4c4
amillert <devmood.business@gmail.com>
2022-02-21 22:20:44 +0100

    add CORS to routes

--- conseil-api/src/main/scala/tech/cryptonomic/conseil/api/Routing.scala
+++ conseil-api/src/main/scala/tech/cryptonomic/conseil/api/Routing.scala
@@ -3,6 +3,9 @@ package tech.cryptonomic.conseil.api
 import cats.effect.IO
 import cats.syntax.option._
 import org.http4s.HttpApp
+import org.http4s.Method._
+import org.http4s.server.middleware.CORS
+import org.typelevel.ci.CIString
 import sttp.model.StatusCode
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe._
@@ -15,17 +18,29 @@ object Routing {
   import tech.cryptonomic.conseil.api.info.model._
   import tech.cryptonomic.conseil.api.info.converters._
 
+  private val allowedCorsVerbs = Set(CONNECT, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE)
+  private val allowedCorsHeaders = Set("X-Requested-With", "content-type", "origin", "accept", "apiKey")
+    .map(CIString.apply)
+
+  val corsPolicy =
+    CORS.policy.withAllowOriginAll
+      .withAllowMethodsIn(allowedCorsVerbs)
+      .withAllowHeadersIn(allowedCorsHeaders)
+      .withAllowCredentials(true)
+
   def instance(api: ConseilApi): HttpApp[IO] =
-    Http4sServerInterpreter[IO](
-      Http4sServerOptions
-        .customInterceptors[IO, IO]
-        .exceptionHandler { _ =>
-          ValuedEndpointOutput(
-            jsonBody[GenericServerError].and(statusCode(StatusCode.InternalServerError)),
-            GenericServerError("serverResource failed")
-          ).some
-        }
-        .options
-    ).toRoutes(api.route).orNotFound
+    corsPolicy.apply(
+      Http4sServerInterpreter[IO](
+        Http4sServerOptions
+          .customInterceptors[IO, IO]
+          .exceptionHandler { _ =>
+            ValuedEndpointOutput(
+              jsonBody[GenericServerError].and(statusCode(StatusCode.InternalServerError)),
+              GenericServerError("serverResource failed")
+            ).some
+          }
+          .options
+      ).toRoutes(api.route).orNotFound
+    )
 
 }
