576b02c39b5cb781d1b046f6bd906f306e377858
amillert <devmood.business@gmail.com>
2022-02-22 10:40:40 +0100

    use all platforms' endpoints in swagger api docs

--- conseil-api/src/main/scala/tech/cryptonomic/conseil/api/APIDocs.scala
+++ conseil-api/src/main/scala/tech/cryptonomic/conseil/api/APIDocs.scala
@@ -4,26 +4,25 @@ import cats.effect.IO
 import sttp.tapir.docs.openapi.OpenAPIDocsInterpreter
 import sttp.tapir.openapi.circe.yaml._
 import sttp.tapir.openapi.OpenAPI
-import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.swagger.SwaggerUI
 
 import tech.cryptonomic.conseil.api.platform.discovery.PlatformDiscoveryEndpoints
 import tech.cryptonomic.conseil.api.platform.data.tezos.TezosDataEndpoints
-import tech.cryptonomic.conseil.api.platform.data.ethereum.EthereumDataEndpoints
+import tech.cryptonomic.conseil.api.platform.data.ethereum.{EthereumDataEndpoints, QuorumDataEndpoints}
 import tech.cryptonomic.conseil.api.platform.data.bitcoin.BitcoinDataEndpoints
 
 trait APIDocs
     extends PlatformDiscoveryEndpoints
+    with TezosDataEndpoints
     with EthereumDataEndpoints
-    // with QuorumDataEndpoints
-    with BitcoinDataEndpoints
-    with TezosDataEndpoints {
+    with QuorumDataEndpoints
+    with BitcoinDataEndpoints {
 
   private lazy val allPlatformEndpoints =
-    discoveryEndpoints ++ xtzEndpoints ++ ethEndpoints /* ++ quorumEndpoints */ ++ btcEndpoints
+    discoveryEndpoints ++ xtzEndpoints ++ ethEndpoints ++ quorumEndpoints ++ btcEndpoints
 
   private lazy val docs: OpenAPI =
     OpenAPIDocsInterpreter().toOpenAPI(protocol.appInfo :: allPlatformEndpoints, "Conseil API", "1.0")
-  lazy val docsRoute: List[ServerEndpoint[Any, IO]] = SwaggerUI[IO](docs.toYaml)
+  lazy val docsRoute = SwaggerUI[IO](docs.toYaml)
 
 }
