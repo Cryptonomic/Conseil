9875f7d1d6c24ea18ac9ac154e553689a2fb024d
amillert <devmood.business@gmail.com>
2022-02-22 10:36:21 +0100

    remove unused Validation trait

--- conseil-api-shared/src/main/scala/tech/cryptonomic/conseil/api/validation/Validation.scala
+++ conseil-api-shared/src/main/scala/tech/cryptonomic/conseil/api/validation/Validation.scala
@@ -1,17 +1,8 @@
 package tech.cryptonomic.conseil.api.validation
 
 import tech.cryptonomic.conseil.common.generic.chain.DataTypes.QueryValidationError
-import org.http4s.Response
 
-/** Trait adding validation for query */
-trait Validation {
-  // self: algebra.Responses =>
-  import Validation.QueryValidating
-
-  /** Method for validating query request */
-  // def validated[A](response: Response[A]): Response[QueryValidating[A]]
-}
-
+/** Object adding validation for query */
 object Validation {
   type QueryValidating[A] = Either[List[QueryValidationError], A]
 }
